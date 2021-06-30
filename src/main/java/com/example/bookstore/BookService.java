7544b78b8febf05525ff3c9b76ec3a39bf5a9af9
Worapat Tubtimdee <worapat.t@kbtg.tech>
2021-06-30 15:46:08 +0700

    [feat] calculate price with largest grouping priority

--- src/main/java/com/example/bookstore/BookService.java
+++ src/main/java/com/example/bookstore/BookService.java
@@ -48,6 +48,34 @@ public class BookService {
 
     private int calculatePrice(List<Integer> totalBuy){
         int price = 0;
+        List<Integer> grouped = new ArrayList<>();
+        List<Integer> tempTotalBuy = totalBuy; //todo (we need all possible cases)
+        int groupingSize = totalBuy.size();
+        //start with largest group as possible then reduce
+        while(groupingSize>1){
+            // minus each volume by 1 until reach grouping count
+                for(int volume=0;volume<groupingSize;volume++){
+                    totalBuy.set(volume, totalBuy.get(volume) - 1);
+                };
+                // when volume reach 0, remove from totalBuy
+                for(int volume=0;volume<totalBuy.size();volume++) {
+                    if (totalBuy.get(volume) == 0) {totalBuy.remove(volume);}
+                };
+                // record when grouping has finished
+                grouped.add(groupingSize);
+                // new groupSize is equal to current totalBut Size
+                groupingSize = totalBuy.size();
+        }
+        //now totalBuy remain only a volume, append count of this volume as 1
+        for(int i=0;i<totalBuy.get(0);i++){grouped.add(1);}
+        System.out.println("G"+grouped);
+        for(Integer group: grouped){
+            price += group == 5? (5 * 0.75 * 100)
+                    :group == 4? (4 * 0.8 * 100)
+                    :group == 3? (3 * 0.9 * 100)
+                    :group == 2? (2 * 0.95 * 100)
+                    :(1 * 100);
+        }
         return price;
     }
 }
